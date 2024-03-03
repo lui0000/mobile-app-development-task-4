d98acc5149d28590901e7b2bcb7d59f1ac9b4b31
Lisa <elbondarenko04@gmail.com>
2024-03-03 21:32:16 +0300

    ScrollView program realization

--- app/src/main/java/com/example/mobile_app_development_task_4/MainActivity.java
+++ app/src/main/java/com/example/mobile_app_development_task_4/MainActivity.java
@@ -2,49 +2,23 @@ package com.example.mobile_app_development_task_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.TypedValue;
-import android.widget.Button;
-import android.widget.GridLayout;
-
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        GridLayout gridLayout = new GridLayout( this);
-        // количество строк
-        gridLayout.setRowCount(3);
-        // количество столбцов
-        gridLayout.setColumnCount(3);
-        for(int i = 1; i <=3; i++){
-            Button btn = new Button(this);
-            btn.setText(String.valueOf(i));
-            gridLayout.addView(btn);
-        }
-        Button btn4 = new Button(this);
-        btn4.setText("4");
-        GridLayout.LayoutParams layoutParams4 = new
-                GridLayout.LayoutParams();
-        layoutParams4.columnSpec = GridLayout.spec(0,2);
-        layoutParams4.width = (int) TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP, 180,
-                getResources().getDisplayMetrics());
-        gridLayout.addView(btn4, layoutParams4);
-        Button btn5 = new Button(this);
-        btn5.setText("5");
-        GridLayout.LayoutParams layoutParams5 = new
-                GridLayout.LayoutParams();
-        layoutParams5.rowSpec = GridLayout.spec(1,2);
-        layoutParams5.height = (int) TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP, 100,
-                getResources().getDisplayMetrics());
-        gridLayout.addView(btn5, layoutParams5);
-        Button btn6 = new Button(this);
-        btn6.setText("6");
-        Button btn7 = new Button(this);
-        btn7.setText("7");
-        gridLayout.addView(btn6);
-        gridLayout.addView(btn7);
-
-        setContentView(gridLayout);
+        //setContentView(R.layout.activity_main);
+        ScrollView scrollView = new ScrollView(this);
+        TextView textView = new TextView(this);
+        textView.setText("Lorem Ipsum is simply dummy text of the printing and typesetting industry...like Aldus PageMaker including versions of Lorem Ipsum.");
+        textView.setLayoutParams(new ViewGroup.LayoutParams
+                (ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+        textView.setTextSize(60);
+        scrollView.addView(textView);
+        setContentView(scrollView);
     }
 }
