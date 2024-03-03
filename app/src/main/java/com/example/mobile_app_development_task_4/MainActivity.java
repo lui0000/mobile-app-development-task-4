1b61c1330c7acf5798bb1fe8f4a9430667c0a89d
Lisa <elbondarenko04@gmail.com>
2024-03-03 21:13:13 +0300

    GridLayout program realization

--- app/src/main/java/com/example/mobile_app_development_task_4/MainActivity.java
+++ app/src/main/java/com/example/mobile_app_development_task_4/MainActivity.java
@@ -1,34 +1,50 @@
 package com.example.mobile_app_development_task_4;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
+import android.widget.GridLayout;
 
-
-import android.os.Bundle;
-import android.view.Gravity;
-import android.widget.FrameLayout;
-import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FrameLayout frameLayout = new FrameLayout(this);
-        TextView textView = new TextView(this);
-        textView.setText("Hello World!");
-        FrameLayout.LayoutParams layoutParams = new
-                FrameLayout.LayoutParams
-                (FrameLayout.LayoutParams.WRAP_CONTENT,
-                        FrameLayout.LayoutParams.WRAP_CONTENT);
-        layoutParams.gravity = Gravity.CENTER_HORIZONTAL | Gravity.TOP;
-        textView.setLayoutParams(layoutParams);
-        textView.setTextSize(26);
-        frameLayout.addView(textView);
-        setContentView(frameLayout);
+        GridLayout gridLayout = new GridLayout( this);
+        // количество строк
+        gridLayout.setRowCount(3);
+        // количество столбцов
+        gridLayout.setColumnCount(3);
+        for(int i = 1; i <=3; i++){
+            Button btn = new Button(this);
+            btn.setText(String.valueOf(i));
+            gridLayout.addView(btn);
+        }
+        Button btn4 = new Button(this);
+        btn4.setText("4");
+        GridLayout.LayoutParams layoutParams4 = new
+                GridLayout.LayoutParams();
+        layoutParams4.columnSpec = GridLayout.spec(0,2);
+        layoutParams4.width = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 180,
+                getResources().getDisplayMetrics());
+        gridLayout.addView(btn4, layoutParams4);
+        Button btn5 = new Button(this);
+        btn5.setText("5");
+        GridLayout.LayoutParams layoutParams5 = new
+                GridLayout.LayoutParams();
+        layoutParams5.rowSpec = GridLayout.spec(1,2);
+        layoutParams5.height = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 100,
+                getResources().getDisplayMetrics());
+        gridLayout.addView(btn5, layoutParams5);
+        Button btn6 = new Button(this);
+        btn6.setText("6");
+        Button btn7 = new Button(this);
+        btn7.setText("7");
+        gridLayout.addView(btn6);
+        gridLayout.addView(btn7);
+
+        setContentView(gridLayout);
     }
 }
